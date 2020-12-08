7118265ddbe06593a752c112e11ef8ab9dc31780
Pampafor <pampafor@users.noreply.github.com>
2020-12-07 21:52:44 -0300

    só indentei os arquivos
    talvez depois a gente devesse tirar um tempo pra fazer um css pra cada parte da página

--- barbearia_xavier/content.php
+++ barbearia_xavier/content.php
@@ -1,267 +1,277 @@
-<div id="demo" class="carousel slide" data-ride="carousel">
+    <div id="demo" class="carousel slide" data-ride="carousel">
 
-    <!-- Indicators -->
-    <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
-    </ul>
-  
-    <!-- The slideshow -->
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="imagens\img1.jpg" alt="Los Angeles">
-      </div>
-      <div class="carousel-item">
-        <img src="imagens\img2.jpg" alt="Chicago">
-      </div>
-      <div class="carousel-item">
-        <img src="imagens\img3.jpg" alt="New York">
-      </div>
-    </div>
-  
-    <!-- Left and right controls -->
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </a>
-  
-  </div>
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
 
-
-  <div id="sobre">
-
-<br/>
-<center><h1> Bem vindo! </h1></center>
-<br/>
-<br/>
-
-<div class = "row about">
-    <div class = "col-1"> </div>
-    <div class = "col-5"> 
-        <center><h3> Conheça nosso salão </h3></center>
-        <hr>
-        <p> A Barbearia Xavier se destaca na cidade de Guarulhos e no bairro dos Pimentas, por trazer um lugar relaxante
-        que busca oferecer um serviço dedicado e de qualidade.
-        </p> 
-        <br/>
-        <p> Inspirada nas barbearias retrôs Nova Iorquinas das décadas de 40, 50 e 60, que eram locais populares para socialização maculina,
-        oferecendo cortes de cabelo, charutos e bebidas.
-        </p>
-        <br/>
-        <p> A Barbearia Xavier oferece aos seus clientes uma experiência customizada de cortes e bebidas, além de barbeiros
-        altamente qualificados.
-        </p>
-    </div>
-    <div class = "col-5"> 
-        <img src="imagens/sobre.jpg" class="img-thumbnail" alt="Cinque Terre">
-    </div>
-    <div class = "col-1"> </div>
-</div>
-
-<br/>
-<br/>
-<br/>
-
-    <div class="servico bg-dark"> 
-    <div class="row"> 
-      <div class="col-1"> </div>
-      <div class="col-10">
-        <br/> 
-        <center><h3> Conheça os nossos serviços </h3></center>
-        <hr> 
-      </div>
-      <div class="col-1"> </div>
-    </div>
-
-    <div class="row"> 
-      <div class="col-2"> </div>
-
-      <div class="col-2"> 
-        <div class="card zoom text-center" style="width:200px height:343px;">
-          <img class="card-img-top" style="height:180px;" src="imagens/tesoura.jpg" alt="Card image" />
-          <div class="card-body border-top border-dark">
-            <h5 class="card-title" style="color:black">Cortes</h5>
-            <p style="color:black"> Dê aquele trato no visual com aquele corte na régua</p>
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="imagens\img1.jpg" alt="Los Angeles">
+          </div>
+          <div class="carousel-item">
+            <img src="imagens\img2.jpg" alt="Chicago">
+          </div>
+          <div class="carousel-item">
+            <img src="imagens\img3.jpg" alt="New York">
           </div>
         </div>
+
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+
       </div>
-      <div class="col-2"> 
-        <div class="card zoom text-center" style="width:200px; height:343px;">
-          <img class="card-img-top" style="height:180px;" src="imagens/navalha.jpg" alt="Card image" />
-          <div class="card-body border-top border-dark">
-            <h5 class="card-title" style="color:black">Barbas</h5>
-            <p style="color:black"> Mostre o seu estilo masculino com uma barba bem feita</p>
-          </div>
+
+        <!-- SEÇÃO SOBRE -->
+
+      <div id="sobre">
+
+    <br/>
+    <center><h1> Bem vindo! </h1></center>
+    <br/>
+    <br/>
+
+    <div class = "row about">
+        <div class = "col-1"> </div>
+        <div class = "col-5"> 
+            <center><h3> Conheça nosso salão </h3></center>
+            <hr>
+            <p> A Barbearia Xavier se destaca na cidade de Guarulhos e no bairro dos Pimentas, por trazer um lugar relaxante
+            que busca oferecer um serviço dedicado e de qualidade.
+            </p> 
+            <br/>
+            <p> Inspirada nas barbearias retrôs Nova Iorquinas das décadas de 40, 50 e 60, que eram locais populares para socialização maculina,
+            oferecendo cortes de cabelo, charutos e bebidas.
+            </p>
+            <br/>
+            <p> A Barbearia Xavier oferece aos seus clientes uma experiência customizada de cortes e bebidas, além de barbeiros
+            altamente qualificados.
+            </p>
         </div>
-      </div>
-      <div class="col-2"> 
-        <div class="card zoom text-center" style="width:200px; height:343px;">
-          <img class="card-img-top" style="height:180px;" src="imagens/corte.jpg" alt="Card image" />
-          <div class="card-body border-top border-dark">
-            <h5 class="card-title" style="color:black">Penteados</h5>
-            <p style="color:black"> Deixe o seu penteado alinhado para qualquer rolê</p>
-          </div>
+        <div class = "col-5"> 
+            <img src="imagens/sobre.jpg" class="img-thumbnail" alt="Cinque Terre">
         </div>
-      </div>
-      <div class="col-2"> 
-        <div class="card zoom text-center" style="width:200px; height:343px;">
-          <img class="card-img-top" style="height:180px;" src="imagens/pomada.jpg" alt="Card image" />
-          <div class="card-body border-top border-dark">
-            <h5 class="card-title" style="color:black">Pomadas</h5>
-            <p style="color:black"> Nossas pomadas dão um estilo mesmo fora da barbearia</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-2"> </div>
+        <div class = "col-1"> </div>
     </div>
+
     <br/>
     <br/>
     <br/>
-    </div>
-    <div id="portfolio">
-    <br />
-    <br />
-    <br />
 
-    
-        <div class="row">
+          <!-- SEÇÃO SERVIÇOS -->
+
+        <div class="servico bg-dark"> 
+        <div class="row"> 
           <div class="col-1"> </div>
-          <div class="col-10"> 
-            <div class="row"> 
-              <div class="col-3"> 
-                <div class="content">
-                  <img src="imagens/galeria1.jpeg" class="rounded" width="250" height="250"/>
-                </div>
+          <div class="col-10">
+            <br/> 
+            <center><h3> Conheça os nossos serviços </h3></center>
+            <hr> 
+          </div>
+          <div class="col-1"> </div>
+        </div>
+
+        <div class="row"> 
+          <div class="col-2"> </div>
+
+          <div class="col-2"> 
+            <div class="card zoom text-center" style="width:200px height:343px;">
+              <img class="card-img-top" style="height:180px;" src="imagens/tesoura.jpg" alt="Card image" />
+              <div class="card-body border-top border-dark">
+                <h5 class="card-title" style="color:black">Cortes</h5>
+                <p style="color:black"> Dê aquele trato no visual com aquele corte na régua</p>
               </div>
-              <div class="col-3">
-                <div class="content">
-                  <img src="imagens/galeria2.jpeg" class="rounded" width="250" height="250"/>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="content">
-                  <img src="imagens/galeria3.jpeg" class="rounded" width="250" height="250"/>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="content">
-                  <img src="imagens/galeria4.jpeg" class="rounded" width="250" height="250"/>
-                </div>
-              </div>  
             </div>
           </div>
-      
-          <div class="col-1"> </div>
+          <div class="col-2"> 
+            <div class="card zoom text-center" style="width:200px; height:343px;">
+              <img class="card-img-top" style="height:180px;" src="imagens/navalha.jpg" alt="Card image" />
+              <div class="card-body border-top border-dark">
+                <h5 class="card-title" style="color:black">Barbas</h5>
+                <p style="color:black"> Mostre o seu estilo masculino com uma barba bem feita</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-2"> 
+            <div class="card zoom text-center" style="width:200px; height:343px;">
+              <img class="card-img-top" style="height:180px;" src="imagens/corte.jpg" alt="Card image" />
+              <div class="card-body border-top border-dark">
+                <h5 class="card-title" style="color:black">Penteados</h5>
+                <p style="color:black"> Deixe o seu penteado alinhado para qualquer rolê</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-2"> 
+            <div class="card zoom text-center" style="width:200px; height:343px;">
+              <img class="card-img-top" style="height:180px;" src="imagens/pomada.jpg" alt="Card image" />
+              <div class="card-body border-top border-dark">
+                <h5 class="card-title" style="color:black">Pomadas</h5>
+                <p style="color:black"> Nossas pomadas dão um estilo mesmo fora da barbearia</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-2"> </div>
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        </div>
 
+          <!-- SEÇÃO PORTFÓLIO -->
+
+        <div id="portfolio">
+        <br />
+        <br />
+        <br />
+
+
+            <div class="row">
+              <div class="col-1"> </div>
+              <div class="col-10"> 
+                <div class="row"> 
+                  <div class="col-3"> 
+                    <div class="content">
+                      <img src="imagens/galeria1.jpeg" class="rounded" width="250" height="250"/>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="content">
+                      <img src="imagens/galeria2.jpeg" class="rounded" width="250" height="250"/>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="content">
+                      <img src="imagens/galeria3.jpeg" class="rounded" width="250" height="250"/>
+                    </div>
+                  </div>
+                  <div class="col-3">
+                    <div class="content">
+                      <img src="imagens/galeria4.jpeg" class="rounded" width="250" height="250"/>
+                    </div>
+                  </div>  
+                </div>
+              </div>
+
+              <div class="col-1"> </div>
+
+          </div>
+
+          <br />
+
+          <div class="row">
+            <div class="col-1"> </div>
+            <div class="col-10"> 
+              <div class="row"> 
+                <div class="col-3"> 
+                  <div class="content">
+                    <img src="imagens/galeria5.jpeg" class="rounded" width="250" height="250"/>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="content">
+                    <img src="imagens/galeria6.jpeg" class="rounded" width="250" height="250"/>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="content">
+                    <img src="imagens/galeria7.jpeg" class="rounded" width="250" height="250"/>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="content">
+                    <img src="imagens/galeria8.jpeg" class="rounded" width="250" height="250"/>
+                  </div>
+                </div>  
+              </div>
+            </div>
+
+            <div class="col-1"> </div>
+
+        </div>
       </div>
 
-      <br />
-      
-      <div class="row">
+    <br/>
+
+          <!-- SEÇÃO EQUIPE -->
+
+      <div class="barbeiro bg-dark" style="color: white">
+
+        <div class="row"> 
+
         <div class="col-1"> </div>
+
         <div class="col-10"> 
-          <div class="row"> 
-            <div class="col-3"> 
-              <div class="content">
-                <img src="imagens/galeria5.jpeg" class="rounded" width="250" height="250"/>
+          <br/>
+          <h6> Conheça </h6>
+          <h3> A nossa equipe </h3>
+          <hr>
+
+          <div class="flip-card" style="display: block; margin-left: auto; margin-right: auto;">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <img src="imagens/barbeiro.jpeg" alt="Avatar" style="width:300px;height:300px;">
+              </div>
+              <div class="flip-card-back bg-white" style="color:black">
+                <h1>Kléber Xavier</h1>
+                <button type="button" class="btn btn-dark">Agende um horário</button>
               </div>
             </div>
-            <div class="col-3">
-              <div class="content">
-                <img src="imagens/galeria6.jpeg" class="rounded" width="250" height="250"/>
-              </div>
-            </div>
-            <div class="col-3">
-              <div class="content">
-                <img src="imagens/galeria7.jpeg" class="rounded" width="250" height="250"/>
-              </div>
-            </div>
-            <div class="col-3">
-              <div class="content">
-                <img src="imagens/galeria8.jpeg" class="rounded" width="250" height="250"/>
-              </div>
-            </div>  
           </div>
-        </div>
+          </div>
 
         <div class="col-1"> </div>
+      </div>
 
-    </div>
-  </div>
-
-<br />
-
-  <div class="barbeiro bg-dark" style="color: white">
-    
-    <div class="row"> 
-
-    <div class="col-1"> </div>
-
-    <div class="col-10"> 
       <br/>
-      <h6> Conheça </h6>
-      <h3> A nossa equipe </h3>
-      <hr>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
 
-      <div class="flip-card" style="display: block; margin-left: auto; margin-right: auto;">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-            <img src="imagens/barbeiro.jpeg" alt="Avatar" style="width:300px;height:300px;">
-          </div>
-          <div class="flip-card-back bg-white" style="color:black">
-            <h1>Kléber Xavier</h1>
-            <button type="button" class="btn btn-dark">Agende um horário</button>
-          </div>
+      </div>
+
+          <!-- SEÇÃO FOOTER -->
+
+      <div class="footer">
+
+        <br/>
+        <br/>
+        <br/>
+
+        <h4><center> Quer receber descontos e novidades? </center></h4>
+        <h2><center> Nos acompanhe nas redes sociais </center></h2>
+        <hr style="border-width: 2px; background-color: darkgray;">
+
+        <div class='socials' style="text-align: center;"> 
+              <div class='socials' style="text-align: center;"> 
+          <a href="" target="_blank">
+            <i style='font-size:50px; color:dodgerblue;' class='fab mx-3 zoom facebook'>&#xf09a;</i>
+          </a>
+
+          <a href="" target="_blank">
+            <i style='font-size:50px;
+            background:-moz-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
+            background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
+            background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+            -webkit-background-clip: text;
+            -moz-background-clip: text;
+            background-clip: text;
+           -webkit-text-fill-color:transparent;' class='fab mx-3 zoom instagram'>&#xf16d;</i>
+          </a>
+
+          <a href="" target="_blank">
+            <i style='font-size:50px; color:lime;' class='fab mx-3 zoom whatsapp'>&#xf232;</i>
+          </a>
         </div>
+
       </div>
-      </div>
-
-    <div class="col-1"> </div>
-  </div>
-
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-  <br/>
-
-  </div>
-
-  <div class="footer">
-
-    <br/>
-    <br/>
-    <br/>
-
-    <h4><center> Quer receber descontos e novidades? </center></h4>
-    <h2><center> Nos acompanhe nas redes sociais </center></h2>
-    <hr style="border-width: 2px; background-color: darkgray;">
-
-    <div class='socials' style="text-align: center;"> 
-          <div class='socials' style="text-align: center;"> 
-      <a href="" target="_blank">
-        <i style='font-size:50px; color:dodgerblue;' class='fab mx-3 zoom facebook'>&#xf09a;</i>
-      </a>
-        
-      <a href="" target="_blank">
-        <i style='font-size:50px;
-        background:-moz-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
-        background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
-        background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
-        -webkit-background-clip: text;
-        -moz-background-clip: text;
-        background-clip: text;
-       -webkit-text-fill-color:transparent;' class='fab mx-3 zoom instagram'>&#xf16d;</i>
-      </a>
-        
-      <a href="" target="_blank">
-        <i style='font-size:50px; color:lime;' class='fab mx-3 zoom whatsapp'>&#xf232;</i>
-      </a>
-    </div>
-
-  </div>
-  <br/>
+      <br/>
--- barbearia_xavier/footer.php
+++ barbearia_xavier/footer.php
@@ -1,6 +1,6 @@
-  <div class ="deselvolvedor bg-dark" style="color: white;"> 
-    <br/>
-    <p><center> Barbearia Xavier© </center></p>
-    <p><center> Desenvolvido por Plexus Desenvolvedor Web </center></p>
-    <br/>
-  </div>
+      <div class ="deselvolvedor bg-dark" style="color: white;"> 
+        <br/>
+        <p><center> Barbearia Xavier© </center></p>
+        <p><center> Desenvolvido por Plexus Desenvolvedor Web </center></p>
+        <br/>
+      </div>
