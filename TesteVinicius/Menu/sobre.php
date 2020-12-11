7683345846aaebf7dc89979b382b9530058838fa
GustavoMOB <61598107+GustavoMOB@users.noreply.github.com>
2020-12-11 12:14:16 -0300

    about us

--- TesteVinicius/Menu/sobre.php
+++ TesteVinicius/Menu/sobre.php
@@ -10,7 +10,6 @@
                         <h2 class="aboutus-title">About Us</h2>
                         <p class="aboutus-text">Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
                         <p class="aboutus-text">This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem</p>
-                        <a class="aboutus-more" href="#">read more</a>
                     </div>
                 </div>
                 <div class="col-md-5 col-sm-6 col-xs-12">
@@ -18,29 +17,29 @@
                         <div class="feature-box">
                             <div class="clearfix">
                                 <div class="iconset">
-                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                    <span class="glyphicon glyphicon-briefcase blue"></span>
                                 </div>
                                 <div class="feature-content">
                                     <h4>Work with heart</h4>
                                     <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
                                 </div>
                             </div>
-                        </div>
+                        </div><br/>
                         <div class="feature-box">
                             <div class="clearfix">
                                 <div class="iconset">
-                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                    <span class="glyphicon glyphicon-eye-open blue"></span>
                                 </div>
                                 <div class="feature-content">
                                     <h4>Reliable services</h4>
                                     <p>Donec vitae sapien ut libero venenatis faucibu. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt</p>
                                 </div>
                             </div>
-                        </div>
+                        </div><br/>
                         <div class="feature-box">
                             <div class="clearfix">
                                 <div class="iconset">
-                                    <span class="glyphicon glyphicon-cog icon"></span>
+                                    <span class="glyphicon glyphicon-usd blue" ></span>
                                 </div>
                                 <div class="feature-content">
                                     <h4>Great support</h4>
@@ -49,6 +48,9 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12"><br/><br/>
+                  <img class="banner" src="imagens/sobre.jpg">
                 </div>
             </div>
         </div>
