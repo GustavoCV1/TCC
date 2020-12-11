e42845c785ae2a74deba688e6a09dc913b737a76
CostaV12 <57457720+CostaV12@users.noreply.github.com>
2020-12-11 11:13:37 -0300

    footer

--- TesteVinicius/Menu/index.php
+++ TesteVinicius/Menu/index.php
@@ -20,6 +20,7 @@
   <link rel="stylesheet" href="port.css">
   <link rel="stylesheet" href="share.css">
   <link rel="stylesheet" href="form.scss">
+  <link rel="stylesheet" href="footer.css">
 
     <!--Portifólio-->
   <link rel="stylesheet" href="css/base.css">  
@@ -48,6 +49,7 @@
         $("#port").load('port.php');
         $("#form").load('form.php');
         $("#share").load('share.php');
+        $("#footer").load('footer.php');
     }
 </script>
 
@@ -60,6 +62,7 @@
     <div id="port"></div>
     <div id="form"></div>
     <div id="share"></div>
+    <div id="footer"></div>
 
 </body>
 </html>
--- TesteVinicius/Menu/servicos.php
+++ TesteVinicius/Menu/servicos.php
@@ -9,8 +9,8 @@
                     <div class="service-icon">
                         <span><i class="fa fa-scissors"></i></span>
                     </div>
-                    <h3 class="title">Web Design</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+                    <h3 class="title">Cortes</h3>
+                    <p class="description">Dê aquele trato no visual com aquele corte na régua</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -18,8 +18,8 @@
                     <div class="service-icon">
                         <span><i class="fa fa-rocket"></i></span>
                     </div>
-                    <h3 class="title">Web Development</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+                    <h3 class="title">Barbas</h3>
+                    <p class="description">Mostre o seu estilo masculino com uma barba bem feita</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -27,8 +27,8 @@
                     <div class="service-icon">
                         <span><i class="fa fa-mobile"></i></span>
                     </div>
-                    <h3 class="title">Responsive Design</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+                    <h3 class="title">Penteados</h3>
+                    <p class="description">Deixe o seu penteado alinhado para qualquer rolê</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -36,8 +36,8 @@
                     <div class="service-icon">
                         <span><i class="fa fa-users"></i></span>
                     </div>
-                    <h3 class="title">Brand Building</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat fugit quas veniam perferendis repudiandae sequi, dolore quisquam illum.</p>
+                    <h3 class="title">Pomadas</h3>
+                    <p class="description">Nossas pomadas dão um estilo mesmo fora da barbearia</p>
                 </div>
             </div>
         </div>
