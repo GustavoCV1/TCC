0728be1def1ab2a848bca5fb88c9d955c05cd61d
CostaV12 <57457720+CostaV12@users.noreply.github.com>
2020-12-11 12:17:07 -0300

    servicos

--- TesteVinicius/Menu/servicos.php
+++ TesteVinicius/Menu/servicos.php
@@ -10,34 +10,34 @@
                         <span><i class="fa fa-scissors"></i></span>
                     </div>
                     <h3 class="title">Cortes</h3>
-                    <p class="description">Dê aquele trato no visual com aquele corte na régua</p>
+                    <p class="description">Na Barbearia Xavier, você dá aquele trato no visual com aquele corte na régua</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
                 <div class="serviceBox pink">
                     <div class="service-icon">
-                        <span><i class="fa fa-rocket"></i></span>
+                        <span><i class="fa fa-scissors"></i></span>
                     </div>
                     <h3 class="title">Barbas</h3>
-                    <p class="description">Mostre o seu estilo masculino com uma barba bem feita</p>
+                    <p class="description">Com a barba feita na Barbearia Xavier, você mostra o seu estilo masculino com uma barba bem feita</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
                 <div class="serviceBox blue">
                     <div class="service-icon">
-                        <span><i class="fa fa-mobile"></i></span>
+                        <span><i class="fa fa-scissors"></i></span>
                     </div>
                     <h3 class="title">Penteados</h3>
-                    <p class="description">Deixe o seu penteado alinhado para qualquer rolê</p>
+                    <p class="description">Deixe o seu penteado alinhado para qualquer rolê com cortes blindados.</p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
                 <div class="serviceBox purple">
                     <div class="service-icon">
-                        <span><i class="fa fa-users"></i></span>
+                        <span><i class="fa fa-scissors"></i></span>
                     </div>
                     <h3 class="title">Pomadas</h3>
-                    <p class="description">Nossas pomadas dão um estilo mesmo fora da barbearia</p>
+                    <p class="description">Aproveite e compre as nossas pomadas e você poderá dar um estilo no seu cabelo mesmo fora da barbearia</p>
                 </div>
             </div>
         </div>
