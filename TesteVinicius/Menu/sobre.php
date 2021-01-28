6a4e82e5cdef4bae08fdb7e7d14b625d24504a60
GustavoMOB <61598107+GustavoMOB@users.noreply.github.com>
2021-01-28 16:35:34 -0300

    about us

--- TesteVinicius/Menu/sobre.php
+++ TesteVinicius/Menu/sobre.php
@@ -1,58 +1,40 @@
+<link rel="stylesheet" href="./sobre.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 
+<section class="hero-section">
+  <div class="card-grid">
+    <a class="card" href="#">
+      <div class="card__background" style="background-image: url(https://imagens-revista.vivadecora.com.br/uploads/2020/06/tipos-de-madeira-carvalho-madeireira-bernauer.jpg)"></div>
+      <div class="card__content">
+      <h3 class="card__heading">Sobre nós</h3>
+    <p class="card__category">A Barbearia Xavier se destaca no bairro dos Pimentas em Guarulhos, por trazer um lugar relaxante que busca oferecer um serviço dedicado e de qualidade com um ambiente agradável</p>
+      </div>
+    </a>
+    <a class="card" href="#">
+      <div class="card__background" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR106T3XJntZ3XKbxNqbEG7uvSMKoQIMofvnw&usqp=CAU)"></div>
+      <div class="card__content">
+      <h3 class="card__heading">Missão</h3>
+        <p class="card__category">Fornecer serviços e produtos de qualidade para nossos clientes de forma dinâmica e responsiva, com foco no realce de beleza do público masculino, em um ambiente de lazer e socialização agradável, confortável e profissional.</p>
+      </div>
+    </a>
+    <a class="card" href="#">
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__content">
+      <h3 class="card__heading">Valores</h3>
+        <p class="card__category">Adaptação, valorização dos clientes, disponibilidade, comunicação clara e precisa, transparência, contato com clientes, inovação e organização.</p>
+      </div>
+    </li>
+    <a class="card" href="#">
+      <div class="card__background" style="background-image: url(https://seculoxximinas.com.br/fgv/wp-content/uploads/2019/06/visao-sistemica-como-ela-pode-ajudar-na-gestao-do-negocio_18620191040330.png)"></div>
+      <div class="card__content">
+      <h3 class="card__heading">Visão</h3>
+        <p class="card__category">Ser uma barbearia de referência no âmbito de serviços estéticos em Guarulhos, garantindo qualidade, adaptabilidade e satisfação aos nossos clientes.</p>
+      </div>
+    </a>
+  <div>
+</section>
+<!-- partial -->
+  
+</body>
+</html>
 
-<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-
-<div class="aboutus-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="aboutus">
-                        <h2 class="aboutus-title">Sobre nós</h2>
-                        <p class="aboutus-text">A Barbearia Xavier se destaca no bairro dos Pimentas em Guarulhos, por trazer um lugar relaxante que busca oferecer um serviço dedicado e de qualidade com um ambiente agradável.</p>
-                        <p class="aboutus-text">Inspirada nas barbearias retrôs Nova Iorquinas das décadas de 40, 50 e 60, que eram locais populares para socialização masculina, oferecendo cortes de cabelo, lugar para fumar e bebidas.</p>
-                        <p class="aboutus-text">A Barbearia Xavier oferece aos seus clientes uma experiência customizada de cortes e bebidas, além de barbeiros altamente qualificados.</p>
-                    </div>
-                </div>
-                <div class="col-md-5 col-sm-6 col-xs-12">
-                    <div class="feature">
-                        <div class="feature-box">
-                            <div class="clearfix">
-                                <div class="iconset">
-                                    <span class="glyphicon glyphicon-briefcase blue"></span>
-                                </div>
-                                <div class="feature-content">
-                                    <h4>Missão</h4>
-                                    <p>Fornecer serviços e produtos de qualidade para nossos clientes de forma dinâmica e responsiva, com foco no realce de beleza do público masculino, em um ambiente de lazer e socialização agradável, confortável e profissional.</p>
-                                </div>
-                            </div>
-                        </div><br/>
-                        <div class="feature-box">
-                            <div class="clearfix">
-                                <div class="iconset">
-                                    <span class="glyphicon glyphicon-eye-open blue"></span>
-                                </div>
-                                <div class="feature-content">
-                                    <h4>Visão</h4>
-                                    <p>Ser uma barbearia de referência no âmbito de serviços estéticos em Guarulhos, garantindo qualidade, adaptabilidade e satisfação aos nossos clientes.</p>
-                                </div>
-                            </div>
-                        </div><br/>
-                        <div class="feature-box">
-                            <div class="clearfix">
-                                <div class="iconset">
-                                    <span class="glyphicon glyphicon-usd blue" ></span>
-                                </div>
-                                <div class="feature-content">
-                                    <h4>Valores</h4>
-                                    <p>Adaptação, valorização dos clientes, disponibilidade, comunicação clara e precisa, transparência, contato com clientes, inovação e organização.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12"><br/><br/>
-                  <img class="banner" src="imagens/sobre.jpg">
-                </div>
-            </div>
-        </div>
-    </div>
