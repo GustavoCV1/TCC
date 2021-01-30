8b63413a9ba9206d67c3147b168f0385597d4da2
GustavoMOB <61598107+GustavoMOB@users.noreply.github.com>
2021-01-30 20:37:03 -0300

    imagens dos valores

--- barbearia_xavier/sobre.php
+++ barbearia_xavier/sobre.php
@@ -4,21 +4,21 @@
 <section class="hero-section">
   <div class="card-grid">
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://imagens-revista.vivadecora.com.br/uploads/2020/06/tipos-de-madeira-carvalho-madeireira-bernauer.jpg)"></div>
+      <div class="card__background" style="background-image: url(https://www.mgpconsultoria.com.br/wp-content/uploads/2017/01/CHAMADO-5-LIDERAN%C3%87A-1.png)"></div>
       <div class="card__content">
       <h3 class="card__heading">Sobre nós</h3>
     <p class="card__category">A Barbearia Xavier se destaca no bairro dos Pimentas em Guarulhos, por trazer um lugar relaxante que busca oferecer um serviço dedicado e de qualidade com um ambiente agradável</p>
       </div>
     </a>
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR106T3XJntZ3XKbxNqbEG7uvSMKoQIMofvnw&usqp=CAU)"></div>
+      <div class="card__background" style="background-image: url(https://www.foodservicenews.com.br/wp-content/uploads/2019/08/fala_professor.png)"></div>
       <div class="card__content">
       <h3 class="card__heading">Missão</h3>
         <p class="card__category">Fornecer serviços e produtos de qualidade para nossos clientes de forma dinâmica e responsiva, com foco no realce de beleza do público masculino, em um ambiente de lazer e socialização agradável, confortável e profissional.</p>
       </div>
     </a>
     <a class="card" href="#">
-      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__background" style="background-image: url(https://sindilegis.org.br/wp-content/uploads/2020/03/autorregulacao_bancos_consignado-900x506.png)"></div>
       <div class="card__content">
       <h3 class="card__heading">Valores</h3>
         <p class="card__category">Adaptação, valorização dos clientes, disponibilidade, comunicação clara e precisa, transparência, contato com clientes, inovação e organização.</p>
