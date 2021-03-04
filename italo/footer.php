a383de7734907f8be09cff13866a4cd01c728da8
GustavoCV1 <80049669+GustavoCV1@users.noreply.github.com>
2021-03-04 11:55:21 -0300

    Delete italo directory

--- italo/footer.php
+++
@@ -1,6 +0,0 @@
-  <div class ="desenvolvedor bg-dark" style="color: white;">
-    <br/>
-    <center><button onclick="window.scrollTo({top: 0, behavior: 'smooth'});" type="button" class="btn btn-light zoom" title="Voltar ao Topo">☝️</button></center>
-    <p><center> Desenvolvido por Italo Roberto Lui </center></p>
-    <br/>
-  </div>
